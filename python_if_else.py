77c914d948108c40213d00d4f8369a0c6a208e65
Sunny7372 <sunnymishra7372@gmail.com>
2022-12-13 07:37:04 -0800

    commited by sunny

---
+++ python_if_else.py
@@ -0,0 +1,29 @@
+# how to use if else
+x= 10
+y=8
+if(x==y):
+    print(" yes, x is equals to y")
+else:
+    print("no, x is not equal to y")
+    # is it mandatory to use else with if
+# the answer will be no
+
+# let me take example
+a=89
+if(a==89):
+    print(" yes, a is equal to 89")
+
+print(" bye")
+
+#  nested if-else
+marks =88
+if(marks>=90):
+    print("Grade A+")
+elif(marks>=80 and marks<90):
+    print("Grade A")
+elif(marks>=70 and marks<80):
+    print(" Grade B+")
+elif(marks>=60 and marks<70):
+    print(" Grade B")
+else:
+    print("Grade C")
