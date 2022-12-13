327368334054d12bbf49273ba9fa6add68fc1cfe
Sunny7372 <sunnymishra7372@gmail.com>
2022-12-13 07:20:10 -0800

    changed by sunny

---
+++ python_operator.py
@@ -0,0 +1,79 @@
+# numerical operators in python
+
+# + for addition
+# - for substraction
+# * for multiplication
+# / for division with float part also
+# // for division with integer part only
+# ** for power  calculation
+# % for modulus
+
+x=5
+y=3
+print(" addition of x+y =",x+y)
+print("substraction of x-y =",x-y)
+print("multiplication of x*y =", x*y)
+print("division of x/y =",x/y)
+print("division of x//y =",x//y)
+print("modulus of x%y =",x%y)
+print("power of y  on x i.e; x**y =",x**y)
+
+# some operations on string
+str_data = "sunny"
+empty_str =''
+
+# concat operation for string
+full_name = str_data + " " + "mishra"
+print(" fullname is " ,full_name)
+
+# if we can use - as well ? it will not work
+# minus_str = "sunny" - "mishra"
+# print("minus_str =",minus_str)
+
+# multiply with  single *
+mul_str = "sunny" * 3
+print(mul_str)
+
+# Assignment operators
+# = , x=5
+# += , x+=5 -> x= x+5
+# -= , x-=5 -> x= x-5
+# *= , x*=5 -> x= x*5
+# /= , x/=5 -> x= x/5
+# //= , x//=5 -> x= x//5
+# %= , x%=5 -> x= x%5
+
+# Comparison operators(in comparison operators we compare operands value)
+# == , Equals to condition x==y
+# != , Not equals to condition , x!=y
+# > , greater than condition , x>y
+# < , less than condition , x<y
+# >= , greater than equals to condition,x>=y
+# <= , less than equals to condition , x<=y
+
+a =10
+b=5
+print(" result of a==b",a==b)
+print("result of a!=b",a!=b)
+print("result of a>b",a>b)
+print("result of a<b",a<b)
+print("result of a>=b",a>=b)
+print("result of a<=b",a<=b)
+
+# Logical operators(logical check will happen for expression result)
+# and -> return true if both statement are true
+# or -> returns true if one statement will true
+# not -> reverse the result, return false if the result is  true
+m = 10
+k = 8
+print("m>10 and k<10 result",m>10 and n<10)
+print("m>20 or k<10 result", m>20 or k<10)
+print("not(m>20 and k<10) result " , not(m>10 and n<10))
+# not(false and true) -> not(false) -> true
+
+
+
+
+
+
+
